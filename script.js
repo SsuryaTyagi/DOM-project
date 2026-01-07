22f767146279486d122c67968e3aaa93f4b8cead
SsuryaTyagi <2040surya@gmail.com>
2026-01-07 15:56:41 +0530

    dayTwo

--- script.js
+++ script.js
@@ -68,32 +68,104 @@ function todo() {
 
 todo();
 
-const dailyPlanner = document.querySelector(
-  ".daily-planner-fullpage .daily-planner"
-);
-let hours = Array.from(
-  { length: 18 },
-  (_, idx) => `${idx + 6}:00 - ${idx + 7}:00`
-);
-let plannerData = JSON.parse(localStorage.getItem("plannerData")) || {};
+function Dailyplaner() {
+  const dailyPlanner = document.querySelector(
+    ".daily-planner-fullpage .daily-planner"
+  );
+  let hours = Array.from(
+    { length: 18 },
+    (_, idx) => `${idx + 6}:00 - ${idx + 7}:00`
+  );
+  let plannerData = JSON.parse(localStorage.getItem("plannerData")) || {};
 
-let wholeDaySum = "";
-hours.forEach((elem, idx) => {
-  let savedData = plannerData[idx] || "";
-  wholeDaySum += `<div class="planner">
+  let wholeDaySum = "";
+  hours.forEach((elem, idx) => {
+    let savedData = plannerData[idx] || "";
+    wholeDaySum += `<div class="planner">
               <p>${elem}</p>
               <input type="text" id="${idx}" value="${savedData}" placeholder=".... ">
             </div> `;
+  });
+
+  dailyPlanner.innerHTML = wholeDaySum;
+
+  document
+    .querySelectorAll(".daily-planner-fullpage .daily-planner input")
+    .forEach((elem, idx) => {
+      elem.addEventListener("input", () => {
+        // console.log(elem.value);
+        plannerData[elem.id] = elem.value;
+        localStorage.setItem("plannerData", JSON.stringify(plannerData));
+      });
+    });
+}
+Dailyplaner();
+
+async function fetchMotiv() {
+  let motivation = document.querySelector(".motivation2 p");
+  let author = document.querySelector(".motivation2 h3");
+  let res = await fetch("http://api.quotable.io/random");
+  let data = await res.json();
+  console.log(data.content);
+
+  motivation.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+fetchMotiv();
+
+function pomo() {
+  
+  let time = 25 * 60;
+let min = 0;
+let sec = 0;
+let timeId = null;
+let isPaused = true;
+
+const h1 = document.querySelector(".time h1");
+const button = document.querySelectorAll(".time button");
+
+function startTime() {
+  if (timeId) return; 
+
+  timeId = setInterval(() => {
+    if (isPaused) return;
+
+    if (time <= 0) {
+      clearInterval(timeId);
+      timeId = null;
+      h1.innerHTML = "00:00";
+      return;
+    }
+
+    min = Math.floor(time / 60);
+    sec = time % 60;
+
+    h1.innerHTML = `${min}:${sec < 10 ? "0" + sec : sec}`;
+    time--;
+  }, 1000);
+}
+
+startTime();
+
+// Pause
+button[1].addEventListener("click", () => {
+  isPaused = true;
 });
 
-dailyPlanner.innerHTML = wholeDaySum;
+// Start / Resume
+button[0].addEventListener("click", () => {
+  isPaused = false;
+});
 
-document
-  .querySelectorAll(".daily-planner-fullpage .daily-planner input")
-  .forEach((elem, idx) => {
-    elem.addEventListener("input", () => {
-      // console.log(elem.value);
-      plannerData[elem.id] = elem.value;
-      localStorage.setItem("plannerData", JSON.stringify(plannerData));
-    });
-  });
+// Reset
+button[2].addEventListener("click", () => {
+  isPaused = true;
+  time = 25 * 60;
+  h1.innerHTML = "25:00";
+});
+}
+pomo();
+
+
+
+
